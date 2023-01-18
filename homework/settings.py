84cbb7f33bcb8000b903197d9a3937e7ba3259fc
Daniel Munoz <dmunoz215@gmail.com>
2023-01-17 23:22:45 -0600

    Configured Database

--- homework/settings.py
+++ homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e3nnt6wm=$osbb36hq#_eciyoc(g-8l%0t9*4au0p^uwat#7bb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
